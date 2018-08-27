43c71704c07a354c83d44c2284a38070e8878484
498162631@qq.com <yjw666666>
2018-08-27 20:08:27 +0800

    no message

--- phptext/wx_sign.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: text/json;charset=utf-8");
-$callback=empty($_REQUEST['callback'])?"":$_REQUEST['callback'];
-require_once "text.php";
-$jssdk = new JSSDK('wx1aa0cca1a108225c', '2d110fb32776fff5161e2927c57dd4a5');
-$signPackage = $jssdk->GetSignPackage();
-$arr = array("status"=>1,"message"=>"成功","result"=>$signPackage);
-echo json_encode($arr);
